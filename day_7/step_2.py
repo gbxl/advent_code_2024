2d02d3a7ebbd59c100bf4698295d4e20e20fa8f9
Lucas Gerbeaux <lucas.gerbeaux@gmail.com>
2024-12-12 11:16:38 -0800

    Catchup

---
+++ day_7/step_2.py
@@ -0,0 +1,35 @@
+def process(tups):
+    total_sum = 0
+    for total, nums in tups:
+        num = nums.pop(0)
+        if can_equate(total, nums, num):
+            total_sum += total
+    return total_sum
+
+
+def can_equate(total, nums, curr) -> bool:
+    if len(nums) == 0:
+        return curr == total
+    num = nums[0]  # Take the first number without modifying the list
+    rest = nums[1:]  # Create a new list with the remaining elements
+    mult = can_equate(total, rest, curr * num)
+    addit = can_equate(total, rest, curr + num)
+    concat = can_equate(total, rest, int(str(curr) + str(num)))
+    return mult or addit or concat
+
+
+def main():
+    with open('/Users/lucasg/work/advent_code_2024/day_7/input_b.txt', 'r') as file:
+        lines = file.readlines()
+    tups = []
+    for line in lines:
+        res = int(line.split(":")[0])
+        nums = [int(n) for n in line.split(":")[1].strip().split(" ")]
+        
+        tups.append((res, nums))
+    result = process(tups)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
